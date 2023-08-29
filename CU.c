80cf0b5eece698ee32f3694faf5a827c138a66cb
xodnd0507 <24457545yong@gmail.com>
2023-08-29 10:30:03 +0900

    초기 커밋.

---
+++ CU.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#pragma warning(disable:4996)
+//코드업 문제 1026 풀이 (시분초 입력받아 분만 출력하기)
+//main() {
+//	int h, m, s;
+//	scanf("%d:%d:%d", &h, &m, &s);
+//	printf("%d" ,m);
+//	return 0;
+//}
+
+//코드업 문제 1027 풀이 (년월일 입력 받아 형식 바꿔 출력하기)
+//main() {
+//	int y, m, d;
+//	scanf("%d.%d.%d", &y, &m, &d);
+//	printf("%02d-%02d-%04d", d, m, y);
+//	return 0;
+//}
